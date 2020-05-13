387d85952f8d2a062a6db69eeb05dfb8676917a7
igazoo <igazoo.23@gmail.com>
2020-05-13 17:40:00 +0900

    CRUD　index create storeの勉強

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,12 +6,15 @@ use Illuminate\Http\Request;
 
 use App\Models\Test;
 
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
   public function index(){
     $values = Test::all();
+    $tests = DB::table('tests')->select('id')->get();
+    dd($tests);
 
-    
 
     //dd($values);
 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test','TestController@index');
+
+// Route::resource('contacts','ContactFormController')->only([
+//   'index','show'
+// ]);
+
+Route::group(['prefix' => 'contact', 'middleware' =>'auth'],function(){
+  Route::get('index','ContactFormController@index')->name('contact.index');
+  Route::get('create','ContactFormController@create')->name('contact.create');
+  Route::post('store','ContactFormController@store')->name('contact.store');
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
